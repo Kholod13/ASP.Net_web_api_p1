945226b6b622c23e06230000001ecd7a89daf4e7
Kholod13 <111706893+Kholod13@users.noreply.github.com>
2024-06-11 09:32:01 +0200

    recreate proj DATA and create productController

---
+++ Controllers/ProductsController.cs
@@ -0,0 +1,38 @@
+﻿using Data.Data;
+using Data.Data.Entities;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace ASP.Net_web_api_p1.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        private readonly ShopDbContext context;
+        public ProductsController(ShopDbContext context) { 
+            this.context = context;
+        }
+        [HttpGet]
+        public ActionResult Get()
+        {
+            return Ok(context.Products.ToList());
+        }
+        [HttpGet("{id}")]
+        public ActionResult Get(int id)
+        {
+            return Ok(context.Products.Find(id));
+        }
+
+        [HttpPost]
+        public IActionResult Create(Product model)
+        {
+            if (!ModelState.IsValid) return NotFound();
+            context.Products.Add(model);
+            context.SaveChanges();
+
+            return Ok();
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,9 +1,19 @@
+using Data.Data;
+using Microsoft.EntityFrameworkCore;
+using System.Text.Json.Serialization;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
+var connStr = builder.Configuration.GetConnectionString("ShopDbContextConnection") ?? throw new InvalidOperationException("Connection string 'ShopDbContextConnection' not found.");
 builder.Services.AddControllers();
+
+builder.Services.AddControllers().AddJsonOptions(x =>
+    x.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.Preserve);
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+
+builder.Services.AddDbContext<ShopDbContext>(opt => opt.UseSqlServer(connStr));
+
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
